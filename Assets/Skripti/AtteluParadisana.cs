ebfc6317178f34a49e78795af441285882e160dd
Lauris <porinslauris@gmail.com>
2022-02-22 15:48:39 +0200

    Projektu uzsākšana

---
+++ Assets/Skripti/AtteluParadisana.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AtteluParadisana : MonoBehaviour {
+	public GameObject bins;
+	public GameObject lacis;
+	public GameObject tante;
+	public GameObject masina;
+	public GameObject atteluTuretajs;
+	public Sprite[] attelumasivs;
+	public GameObject slaideris;
+		
+	public void binaAttelosana(bool vertiba){
+		bins.SetActive (vertiba);
+	}
+	public void lacisaAttelosana(bool vertiba){
+		lacis.SetActive (vertiba);
+	}
+	public void tantesAttelosana(bool vertiba){
+		tante.SetActive (vertiba);
+	}
+	public void masinasAttelosana(bool vertiba){
+		masina.SetActive (vertiba);
+	}
+	public void paKreisiBins(){
+		bins.transform.localScale = new Vector2 (1, 1);
+	}
+	public void paLabiBins(){
+		bins.transform.localScale = new Vector2(-1,1);
+	}
+	public void izkritosais(int skaitlis){
+		if (skaitlis == 0) {
+			atteluTuretajs.GetComponent<Image> ().sprite = attelumasivs [0];
+		} else if (skaitlis == 1) {
+			atteluTuretajs.GetComponent<Image> ().sprite = attelumasivs [1];
+		} else if (skaitlis == 2) {
+			atteluTuretajs.GetComponent<Image> ().sprite = attelumasivs [2];
+		}
+	}
+	public void mainitLielumu(){
+		float pasreizejaVertiba = slaideris.GetComponent<Slider> ().value;
+		atteluTuretajs.transform.localScale = new Vector2 (1F*pasreizejaVertiba, 1F*pasreizejaVertiba);
+	}
+}
---
+++ Assets/Skripti/KursorsUzAttela.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KursorsUzAttela : MonoBehaviour {
+	public AudioSource skanasAvots;
+	public AudioClip skanaKoAtskanot;
+
+	public void uzbiditsUzAttela(){
+		skanasAvots.PlayOneShot (skanaKoAtskanot);
+	}
+
+	public void nobiditsUzAttela(){
+		skanasAvots.Stop ();
+	}
+
+}
