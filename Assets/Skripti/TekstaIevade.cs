fd5b8ee9893aecfe899d72e9b42835124ad4c9b8
Lauris <porinslauris@gmail.com>
2022-02-23 12:13:47 +0200

    Teksta ievade strāda, var nomainīt attēlus

---
+++ Assets/Skripti/TekstaIevade.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TekstaIevade : MonoBehaviour {
+	public string vards;
+	public string vecums;
+	public GameObject ievadesVecums;
+	public GameObject ievadesVards;
+
+	public GameObject tekstaAttelosanasLauks;
+
+	public void darbibasArTekstu(){
+		vards = ievadesVards.GetComponent<Text> ().text;
+		vecums = ievadesVecums.GetComponent<Text> ().text;
+		tekstaAttelosanasLauks.GetComponent<Text> ().text = "Stīvs vārdā " + vards.ToUpper () + " ir "+vecums+" gadu vecs!";
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Porins_UnityUI
+
+- [ ] Pielietot teksta ievades laukus (2. gab)
+- [ ] Pielietot izkrītošā saraksta pogu
+- [ ] Izveidot apģērbu un aprīkojuma kategorijas
+- [ ] drag un drop princips
+- [ ] Nodrošināt skaņas efektus
+- [ ] Iekļaut 2 slider elementus, kas maina apģerbamā tēla garumu, otrs platumu
+ 
